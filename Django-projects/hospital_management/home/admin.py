309571cf85b2f52b7fdb054bc09ff647152a0b49
Rameela <rameelack@outlook.com>
2022-08-25 12:56:23 +0300

    first commit

---
+++ Django-projects/hospital_management/home/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Departments,Doctors,Booking
+
+# Register your models here.
+admin.site.register(Departments)
+admin.site.register(Doctors)
+
+
+class BookingAdmin(admin.ModelAdmin):
+
+    list_display = ('id','patient_name','patient_phone','patient_email','doc_name','booking_date','booked_on')
+
+
+admin.site.register(Booking,BookingAdmin)
---
+++ Django-projects/hospital_management/home/forms.py
@@ -0,0 +1,35 @@
+from cProfile import label
+from django import forms
+from .models import Booking
+
+
+class DateInput(forms.DateInput):
+    input_type ='date'
+
+
+class BookingForm(forms.ModelForm):
+
+
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
+
+        widgets = {
+            'booking_date' : DateInput()
+        }
+
+
+        labels = {
+
+            'patient_name':"Patient Name: ",
+            'patient_phone' : "Patient Phone: ",
+            'patient_email':"Patient Email:",
+            'doc_name':"Doctor Name: ",
+            'booking_date':"Booking Date: ",
+        }
+
+
+
+
+        
